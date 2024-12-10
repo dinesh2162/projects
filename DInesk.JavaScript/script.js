ae1a0260f0e766e6b1a13c69b97611b588bb4554
dinesh2162 <148421102+dinesh2162@users.noreply.github.com>
2024-12-10 18:53:14 +0530

    Add files via upload

---
+++ DInesk.JavaScript/script.js
@@ -0,0 +1,8 @@
+var hello=document.getElementById("Delete")
+hello.addEventListener("mouseover",function(){
+ alert("Hello👋")   
+})
+var hello2=document.getElementById("button")
+hello2.addEventListener("mouseover",function(){
+    alert("Hii Guys👋")
+})
